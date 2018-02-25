2ff5b5d46ffd8221bc4479f9c82552dc2b66aa69
baonv <baonv@ilandapp.com>
2018-02-25 19:30:48 +0800

    init project

---
+++ SvPayServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SvPayServer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
